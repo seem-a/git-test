df49612fb8c990c394dc23cb3fc853fe0b6e159d
Seema Mehta <seema@seemas-mac.lan>
2013-10-20 16:43:57 +1300

    Added test files

---
+++ Git Test/PATest.h
@@ -0,0 +1,13 @@
+//
+//  PATest.h
+//  Git Test
+//
+//  Created by Seema Mehta on 20/10/13.
+//  Copyright (c) 2013 iDoodle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PATest : NSObject
+
+@end
---
+++ Git Test/PATest.m
@@ -0,0 +1,13 @@
+//
+//  PATest.m
+//  Git Test
+//
+//  Created by Seema Mehta on 20/10/13.
+//  Copyright (c) 2013 iDoodle. All rights reserved.
+//
+
+#import "PATest.h"
+
+@implementation PATest
+
+@end
